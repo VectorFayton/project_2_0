763e8daeece310ba3e3b06dc8edb6b206677be46
Alimkulov Nurbol <124434364+VectorFayton@users.noreply.github.com>
2023-03-27 21:20:17 +0600

    kl

--- src/main/java/project2/LoginMenu.java
+++ src/main/java/project2/LoginMenu.java
@@ -1,11 +1,20 @@
 package project2;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class LoginMenu {
     @FXML
     private Label welcomeText;
+    @FXML
+    private Label registration_button;
 
     @FXML
     protected void onHelloButtonClick() {
@@ -14,5 +23,24 @@ public class LoginMenu {
     @FXML
     protected void onRegisterButtonClicked(){
         welcomeText.setText("Registration");
+        registration_button.getScene().getWindow().hide();
+
+        FXMLLoader loader_registration_menu = new FXMLLoader();
+        loader_registration_menu.setLocation(getClass().getResource("RegistrationMenu.fxml"));
+
+        try {
+            loader_registration_menu.load();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        Parent root = loader_registration_menu.getRoot();
+        Stage stage = new Stage();
+        stage.setScene(new Scene(root));
+        stage.showAndWait();
+    }
+    @FXML
+    protected void onForgotPasswordMouseClick(){
+        welcomeText.setText("ForgotPassword");
     }
 }
